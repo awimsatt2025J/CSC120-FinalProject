14261a6a246f66c483837582d35622820a855a40
sbrainard <sbrainard@smith.edu>
2023-12-07 14:54:18 -0500

    slight changes (mostly comments)

--- GameInput.java
+++ GameInput.java
@@ -8,17 +8,17 @@ public class GameInput {
     private ArrayList <Room> gameMap; //makes an array list of the rooms
     private Person player; //the user
 
-    List<String> commandWords = new ArrayList<>(Arrays.asList("go", "examine", "give", "grab")); //as list of commands the user is allowed to use
+    List<String> commandWords = new ArrayList<>(Arrays.asList("go", "examine", "give", "grab", "use")); //as list of commands the user is allowed to use
     List<String> things = new ArrayList<>(Arrays.asList("coffee", "Jordan's OneCard", "phone")); //a list of objects in the game
 
     public GameInput() {
-        gameMap = new ArrayList <Room> ();
-        gameMap.add(new Room("Ford 241", "CSC 120 Classroom"));
+        gameMap = new ArrayList <Room> (); //creates an array list to store the map of the rooms
+        gameMap.add(new Room("Ford 241", "CSC 120 Classroom")); //adds the CS classroom to the map
 
-        player = new Person("Player", gameMap.get(0));
+        player = new Person("Player", gameMap.get(0)); //adds the player and sets their location at the CS classroom
     }
 
-    private ArrayList getMap() {
+    private ArrayList getMap() { //returns
         return gameMap;
     }
 
